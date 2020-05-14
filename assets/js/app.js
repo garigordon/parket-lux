2a153abd575936721818358ed90eef93e30f1e29
Mykhailo Pugovkin <mykhailo.pugovkin@dev-optima.com>
2020-05-14 19:32:27 +0300

    home page v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,12 +19,39 @@
     };
   }(jQuery);
 
+  var backToTop = function ($) {
+
+    function init() {
+      var offset = 220;
+      var duration = 500;
+      jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() > offset) {
+          jQuery('.back-to-top').fadeIn(duration);
+        } else {
+          jQuery('.back-to-top').fadeOut(duration);
+        }
+      });
+      jQuery('.back-to-top').click(function (event) {
+        event.preventDefault();
+        jQuery('html, body').animate({
+          scrollTop: 0
+        }, duration);
+        return false;
+      });
+    }
+
+    return {
+      init: init
+    };
+  }(jQuery);
+
   // You can write a call and import your functions in this file.
 
   (function ($) {
     // When DOM is ready
     $(function () {
       slider.init();
+      backToTop.init();
     });
   })(jQuery);
 
--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,12 @@
 // Feel free with using ES6 here.
 
 import slider from './modules/slider-home';
+import backToTop from './modules/backToTop';
 
 (($) => {
   // When DOM is ready
   $(() => {
     slider.init();
+    backToTop.init();
   });
 })(jQuery);
