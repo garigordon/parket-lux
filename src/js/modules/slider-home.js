4e68213edea2395ac3e6531409edba93428fd426
Mykhailo Pugovkin <mykhailo.pugovkin@dev-optima.com>
2020-09-05 16:06:54 +0300

    brands, contacts, single pages

--- src/js/modules/slider-home.js
+++ src/js/modules/slider-home.js
@@ -3,12 +3,12 @@ var slider = (function($) {
 
   function init() {
     $(".slider-home").slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		dots: true
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true
     });
   }
 
