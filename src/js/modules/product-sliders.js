583e2a4cb4ef7711411c63285414019425929dc4
Mykhailo Pugovkin <mykhailo.pugovkin@dev-optima.com>
2020-09-24 12:53:02 +0300

    products

--- src/js/modules/product-sliders.js
+++ src/js/modules/product-sliders.js
@@ -1,9 +1,9 @@
 (function ($) {
   $(document).ready(() => {
-    let sliders = []
+    const sliders = [];
     for (let i = 0; i < 10; i++) {
-      let selector = `.js-slider-${i + 1}`
-      if (!$(selector).length) continue
+      const selector = `.js-slider-${i + 1}`;
+      if (!$(selector).length) continue;
 
       sliders.push(window[`slider-${i + 1}`] = new Swiper(selector, {
         slidesPerView: 3,
@@ -11,16 +11,19 @@
         breakpoints: {
           // when window width is <= 320px
           320: {
-            slidesPerView: 2
+            slidesPerView: 1,
           },
           // when window width is <= 480px
           480: {
-            slidesPerView: 2
+            slidesPerView: 2,
           },
           // when window width is <= 640px
           768: {
             slidesPerView: 2,
-          }
+          },
+          1000: {
+            slidesPerView: 3,
+          },
         },
         navigation: {
           nextEl: `.swiper-button-next.js-slider-btn-${i + 1}`,
